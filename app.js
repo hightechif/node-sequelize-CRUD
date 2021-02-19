096036ba3c9a36dc7231b7c2b6ac19f0abaa40b0
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2021-02-19 21:30:59 +0700

    fix db error

--- app.js
+++ app.js
@@ -4,24 +4,24 @@ const methodOverride = require('method-override');
 const app = express();
 const {Article} = require('./models');
 
-const { Client } = require('pg');
+// const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
-client.connect();
+// client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
